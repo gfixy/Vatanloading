df543d32c0690116d9143d36a9b32e1378ed5064
gfixy <bybring@mail.ru>
2021-09-08 09:59:11 +0300

    final sound

--- js/sleek.js
+++ js/sleek.js
@@ -1,3 +1,23 @@
+//Array randomizer (Fisher-Yates algorithm)
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 function setMusicName(name) {
 	$("#music-name").fadeOut(2000, function() {
 		$(this).html(name);
@@ -9,6 +29,9 @@ var youtubePlayer;
 var actualMusic = -1;
 
 $(function() {
+	
+	if (l_musicRandom)
+	l_musicPlaylist = shuffle(l_musicPlaylist);
 
 	if (l_music) {
 		loadYoutube();
