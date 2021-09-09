04073a3054bbc1c22bdb8c6b88bc2de81f1f5f46
gfixy <bybring@mail.ru>
2021-09-09 04:24:39 +0300

    edit

--- js/sleekc.js
+++ js/sleekc.js
@@ -54,7 +54,7 @@ Avaiable options:
 
 You can add custom backgrounds
 Just make sure it is at webm VP8 format */
-var l_background = "backgrounds/videos/Pexels 3840 x 2160.wmv";
+var l_background = "backgrounds/videos/Pexels.webm";
 
 /*(Image-background only)
 Place one or more images
